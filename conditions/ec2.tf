504e17783d32aab7991179cf4f0aa3f5a51b0617
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-17 22:13:58 +0530

    terraform

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -9,14 +9,14 @@ resource "aws_security_group" "allow_ssh_terraform"{
     description="allow SSH traffic from port 22"
 
     egress {
-        rom_port         = 0
+        from_port        = 0
         to_port          = 0
         protocol         = "-1"
         cidr_blocks      = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
   }
     ingress {
-        rom_port         = 22
+        from_port        = 22
         to_port          = 22
         protocol         = "TCP"
         cidr_blocks      = ["0.0.0.0/0"]
