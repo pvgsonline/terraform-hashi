f6ca7cf5ce5737ce6cc43b7d89cb9931927da52c
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-17 15:51:43 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "terraform"{
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+
+    tags = {
+    Name = "terraform"    
+    }
+    
+}
+
+resource "aws_security_group" "allow_ssh_terraform" {
+ name = "allow_sshh"    
+ description = "allows ssh traffic from port no 22"
+
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+    tags = {
+    Name = "allow_sshh"
+  }
+
+}
+
+
