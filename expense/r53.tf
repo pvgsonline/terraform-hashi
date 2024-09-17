72ff5322cb845dff49a5295f0b4a813e0575cdde
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-18 00:48:56 +0530

    terraform

--- expense/r53.tf
+++ expense/r53.tf
@@ -4,5 +4,7 @@ resource "aws_route53_record" "r53_records" {
   name    = "${var.instance_name[count.index]}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = [for instance in aws_instance.terraform : instance.private_ip] 
+  #records = [for instance in aws_instance.terraform : instance.private_ip] 
+  records = [aws_instance.terraform[count.index].private_ip]
+  allow_overwrite = true
 }
