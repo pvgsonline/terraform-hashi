c8a0375a907a13609e42415ffcb20e928ee538dd
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-17 22:08:42 +0530

    terraform

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terraform"{
     ami = "ami-09c813fb71547fc4f"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-    instance_type = var.environment ? prod "t3.small" : "t3.micro"
+    instance_type = var.environment == "prod" ? "t3.small" : "t3.micro"
 }
 
 resource "aws_security_group" "allow_ssh_terraform"{
