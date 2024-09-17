a44923ca86af05dcb0f60a43695a07307317bd15
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-17 22:55:27 +0530

    terraform

--- loops/outputs.tf
+++ loops/outputs.tf
@@ -1,5 +1,5 @@
 output "public_ip" {
-  value       = aws_instance.terraform.public_ip
+  value       = aws_instance.terraform.public_ip.[count.index]
   sensitive   = false
   description = "provides the public ip address"
 }
