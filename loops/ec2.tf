64ad7e5ceb0409312f3dfa142e33ad38bdcee42f
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-17 22:39:17 +0530

    terraform

--- loops/ec2.tf
+++ loops/ec2.tf
@@ -3,9 +3,9 @@ resource "aws_instance" "terraform"{
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-
+    tags = {
     Name = var.instance_name[count.index]
-
+    }
 }
 
 resource "aws_security_group" "allow_ssh_terraform"{
