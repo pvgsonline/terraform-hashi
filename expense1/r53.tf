627a591b19d75758ea7ed53b220fb0448f04e7b6
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-19 00:04:06 +0530

    terraform

---
+++ expense1/r53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "terraform"{
+    zone_id = var.zone_id
+  name    = {"${var.instance_name[count.index]}"}.{"${var.domain_name}"}
+  type    = "A"
+  ttl     = 1
+  records = [var.instance_name[count.index] == "frontend" ? "aws_instance.terraform[count.index].public_ip" : "aws_instance.terraform[count.index].private_ip"]
+}
