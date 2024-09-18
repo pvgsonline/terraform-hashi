e139278009dc7b8ce4d236b900e3bea9bed0aad3
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-19 00:08:11 +0530

    terraform

--- expense1/r53.tf
+++ expense1/r53.tf
@@ -1,4 +1,5 @@
 resource "aws_route53_record" "terraform"{
+    count = length(var.instance_name)  
     zone_id = var.zone_id
   name    = "${var.instance_name[count.index]}.${var.domain_name}"
   type    = "A"
