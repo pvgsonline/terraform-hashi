0e32425f041e3c0f07f79a419eddcbff0ac16a1a
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-22 19:20:12 +0530

    terraform

--- locals/r53.tf
+++ locals/r53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "terraform"{
-    count = length(var.instance_name[count.index])
+    count = length(var.instance_name)
     zone_id = local.zone_id
     name = var.instance_name[count.index] == "frontend" ? local.domain_name : "${var.instance_name[count.index]}.${local.domain_name}"
     type    = "A"
