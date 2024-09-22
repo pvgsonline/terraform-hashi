090ab15f9f488c0cacfe5b1579bc401554afe044
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-22 20:46:16 +0530

    terraform

---
+++ for-each/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "pvgs-remote-state"
+    key = "for-each-demo"
+    region = "us-east-1"
+    dynamodb_table = "pvgs-locking"
+  }
+  
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
