1dbcc951f160562198185d5dee9752afa99e4fa4
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-09-22 22:54:11 +0530

    terraform

--- dynamic/ec2.tf
+++ dynamic/ec2.tf
@@ -23,10 +23,10 @@ resource "aws_security_group" "allow_ssh_terraform" {
   dynamic "ingress" {
     for_each = var.ingress_rules
     content {
-    from_port        = each.value["from_port"]
-    to_port          = each.value["to_port"]
-    protocol         = each.value["protocol"]
-    cidr_blocks      = each.value.cidr_blocks
+    from_port        = ingress.value["from_port"]
+    to_port          = ingress.value["to_port"]
+    protocol         = ingress.value["protocol"]
+    cidr_blocks      = ingress.value.cidr_blocks
   }
   }
 
